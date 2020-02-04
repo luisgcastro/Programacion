8374f8979dbea71d680ed1cd125458d644531576
luisgarciacastro <luisgarciacastro@localhost>
2020-02-04 13:23:32 +0100

    .

---
+++ src/sesion11/ExtraArray_Ej1_Array2D.java
@@ -0,0 +1,30 @@
+package sesion11;
+
+import java.util.Arrays;
+
+public class ExtraArray_Ej1_Array2D {
+	
+	
+	public static void main(String[] args) {
+		int [][] matriz = new int[3][3];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				matriz[i][j]=(i*3)+j;
+				
+			}
+			
+		}
+		mostrarContenidoMatriz(matriz);
+		
+		
+	}
+	
+	public static void mostrarContenidoMatriz(int[][] matriz) {
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.println(Arrays.toString(matriz[i]));
+		}
+	}
+	
+
+}
