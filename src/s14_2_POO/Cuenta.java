7f65ad36d6287cc9018a5e9de28750d8fc947109
luisgarciacastro <luisgarciacastro@localhost>
2020-03-04 12:17:39 +0100

    x

--- src/s14_2_POO/Cuenta.java
+++ src/s14_2_POO/Cuenta.java
@@ -1,5 +1,81 @@
 package s14_2_POO;
 
 public class Cuenta {
+	
+	private String titular;
+	private double cantidad;
+	
+	
+	
+	public Cuenta(String titular) {
+		super();
+		this.titular = titular;
+	}
 
+
+
+	public Cuenta(String titular, double cantidad) {
+		super();
+		this.titular = titular;
+		this.cantidad = cantidad;
+	}
+
+
+
+	/**
+	 * @return the titular
+	 */
+	public String getTitular() {
+		return titular;
+	}
+
+
+
+	/**
+	 * @param titular the titular to set
+	 */
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+
+
+	/**
+	 * @return the cantidad
+	 */
+	public double getCantidad() {
+		return cantidad;
+	}
+
+
+
+	/**
+	 * @param cantidad the cantidad to set
+	 */
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Cuenta [titular=" + titular + ", cantidad=" + cantidad + "]";
+	}
+	
+	public void ingresar(double cantidad) {
+		
+		if (cantidad >=0) {
+			this.cantidad = this.cantidad + cantidad;
+		}
+
+	}
+	public void retirar(double cantidad) {
+		this.cantidad = this.cantidad - cantidad;
+		if (this.cantidad<0) {
+			this.cantidad=0;
+		}
+
+	}
+	
 }
--- src/s14_2_POO/MainCuenta.java
+++ src/s14_2_POO/MainCuenta.java
@@ -3,8 +3,20 @@ package s14_2_POO;
 public class MainCuenta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cuenta cuenta1 = new Cuenta("Manuel");
+		Cuenta cuenta2 = new Cuenta("Pepe", 1.000);
 
-	}
+	
+	System.out.println(cuenta1);
+	System.out.println(cuenta2);
+	
+	System.out.println("--------------------");
+	
+	cuenta1.ingresar(400);
+	System.out.println(cuenta1);
+	
+	cuenta2.retirar(400);
+	System.out.println(cuenta2);
 
+}
 }
